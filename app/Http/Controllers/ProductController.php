534fba1683ea6e346ff03aff9062245a070a9dce
rustamdotdev <thisisrustam@gmail.com>
2023-12-23 10:49:59 +0300

    sending for laptop

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreProductRequest;
+use App\Models\Material;
+use App\Models\Warehouses;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function store(StoreProductRequest $request)
+    {
+        $product_material = null;
+        $product_name = $request['product_name'];
+        $product_qty = $request['product_qty'];
+        $materials = $request['materials'];
+
+        $response = Warehouses::query()
+            ->join('materials', 'materials.id', '=', 'warehouses.material_id')
+            ->where('materials.id', '=', 1)
+            ->get();
+
+        if ($response[0]['reminder'] >= $product_qty) {
+           $response[0]['qty'] = $product_qty;
+        } else {
+            $product_material = $response[0]['reminder'];
+        }
+//        return response()->json('');
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             MaterialSeeder::class,
+            WarehousesSeeder::class,
         ]);
     }
 }
