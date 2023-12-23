dc113d2cd26666e952fbe05fa57a976aa3f8052c
Rustam-Fozilov <fozilovrustam32@gmail.com>
2023-12-23 23:44:44 +0500

    fixed: response order

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,22 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreProductRequest;
-use App\Http\Resources\ProductResource;
 use App\Http\Resources\WarehouseResource;
-use App\Models\Material;
 use App\Models\Warehouses;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function store(StoreProductRequest $request)
+    public function __invoke(StoreProductRequest $request): JsonResponse
     {
-
         $productsData = [];
 
         foreach ($request['products'] as $product) {
-            $productsData[] = [
+            $productData = [
                 'product_name' => $product['product_name'],
                 'product_qty' => $product['product_qty'],
                 'product_materials' => [],
@@ -31,7 +28,7 @@ class ProductController extends Controller
 
                 if ($warehouse['reminder'] < $material['qty']) {
                     $warehouseResource = new WarehouseResource($warehouse);
-                    $productsData[0]['product_materials'][] = $warehouseResource->resolve();
+                    $productData['product_materials'][] = $warehouseResource->resolve();
 
                     $warehouse = Warehouses::with('material')
                         ->where('material_id', '=', $material['id'])
@@ -51,24 +48,26 @@ class ProductController extends Controller
                         ->where('reminder', '<=', $material['qty'])
                         ->first();
 
-                    $productsData[0]['product_materials'][] = [
+                    $productData['product_materials'][] = [
                         'warehouse_id' => null,
                         'material_name' => $warehouse['material']['name'],
-                        'qty' => $material['qty'] - $productsData[0]['product_materials'][count($productsData[0]['product_materials']) - 1]['qty'],
-                        'price' => null
+                        'qty' => $material['qty'] - end($productData['product_materials'])['qty'],
+                        'price' => null,
                     ];
 
                 } else {
-                    $qty = $material['qty'] - $productsData[0]['product_materials'][count($productsData[0]['product_materials']) - 1]['qty'];
+                    $qty = $material['qty'] - end($productData['product_materials'])['qty'];
 
-                    $productsData[0]['product_materials'][] = [
+                    $productData['product_materials'][] = [
                         'warehouse_id' => $warehouse['id'],
                         'material_name' => $warehouse['material']['name'],
                         'qty' => $qty,
-                        'price' => $warehouse['price']
+                        'price' => $warehouse['price'],
                     ];
                 }
             }
+
+            $productsData[] = $productData;
         }
 
         return response($productsData);
--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('product', ProductController::class);
+Route::post('products', ProductController::class);
