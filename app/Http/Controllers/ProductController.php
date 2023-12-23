72b304b106d055f33ce5f285ceaba569fe1a552c
Rustam-Fozilov <fozilovrustam32@gmail.com>
2023-12-24 00:19:32 +0500

    refactoring

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -21,55 +21,73 @@ class ProductController extends Controller
                 'product_materials' => [],
             ];
 
+            /* Mahsulotga tegishli materiallarni qidirish va saqlash */
             foreach ($product['materials'] as $material) {
                 $warehouse = Warehouses::with('material')
                     ->where('material_id', '=', $material['id'])
                     ->first();
 
-                if ($warehouse['reminder'] < $material['qty']) {
-                    $warehouseResource = new WarehouseResource($warehouse);
-                    $productData['product_materials'][] = $warehouseResource->resolve();
+                /* Ombordan materiallarni izlash */
+                list($productData, $warehouse) = $this->checkWarehouse($warehouse, $material, $productData);
 
-                    $warehouse = Warehouses::with('material')
-                        ->where('material_id', '=', $material['id'])
-                        ->where('id', '!=', $warehouse['id'])
-                        ->where('reminder', '>=', $material['qty'])
-                        ->first();
-                }
-
-                if (is_null($warehouse)) {
-                    $warehouse = Warehouses::with('material')
-                        ->where('material_id', '=', $material['id'])
-                        ->first();
-
-                    $warehouse = Warehouses::with('material')
-                        ->where('material_id', '=', $material['id'])
-                        ->where('id', '!=', $warehouse['id'])
-                        ->where('reminder', '<=', $material['qty'])
-                        ->first();
-
-                    $productData['product_materials'][] = [
-                        'warehouse_id' => null,
-                        'material_name' => $warehouse['material']['name'],
-                        'qty' => $material['qty'] - end($productData['product_materials'])['qty'],
-                        'price' => null,
-                    ];
-
-                } else {
-                    $qty = $material['qty'] - end($productData['product_materials'])['qty'];
-
-                    $productData['product_materials'][] = [
-                        'warehouse_id' => $warehouse['id'],
-                        'material_name' => $warehouse['material']['name'],
-                        'qty' => $qty,
-                        'price' => $warehouse['price'],
-                    ];
-                }
+                /* Ombordan topilgan materiallarni saqlash */
+                list($productData) = $this->saveMaterials($warehouse, $material, $productData);
             }
 
             $productsData[] = $productData;
         }
 
-        return response($productsData);
+        return response()->json($productsData);
+    }
+
+    public function checkWarehouse($warehouse, mixed $material, array $productData): array
+    {
+        if ($warehouse['reminder'] < $material['qty']) {
+            $warehouseResource = new WarehouseResource($warehouse);
+            $productData['product_materials'][] = $warehouseResource->resolve();
+
+            $warehouse = Warehouses::with('material')
+                ->where('material_id', '=', $material['id'])
+                ->where('id', '!=', $warehouse['id'])
+                ->where('reminder', '>=', $material['qty'])
+                ->first();
+        }
+
+        return array($productData, $warehouse);
+    }
+
+    public function saveMaterials(mixed $warehouse, mixed $material, mixed $productData): array
+    {
+        if (is_null($warehouse)) {
+            /* Agar ombordan yetarlicha miqdorda material topilmasa bor materialni saqlab qo'yish */
+            $warehouse = Warehouses::with('material')
+                ->where('material_id', '=', $material['id'])
+                ->first();
+
+            $warehouse = Warehouses::with('material')
+                ->where('material_id', '=', $material['id'])
+                ->where('id', '!=', $warehouse['id'])
+                ->where('reminder', '<=', $material['qty'])
+                ->first();
+
+            $productData['product_materials'][] = [
+                'warehouse_id' => null,
+                'material_name' => $warehouse['material']['name'],
+                'qty' => $material['qty'] - end($productData['product_materials'])['qty'],
+                'price' => null,
+            ];
+        } else {
+            /* Ombordan kerakli materiallar topilsa saqlab qo'yish */
+            $qty = $material['qty'] - end($productData['product_materials'])['qty'];
+
+            $productData['product_materials'][] = [
+                'warehouse_id' => $warehouse['id'],
+                'material_name' => $warehouse['material']['name'],
+                'qty' => $qty,
+                'price' => $warehouse['price'],
+            ];
+        }
+
+        return array($productData);
     }
 }
